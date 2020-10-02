38bb85ffef62dd1a2a0efafbca9a236b19aeeaae
Jordan Triplett <JordanT111596@gmail.com>
2020-10-02 14:10:39 -0400

    Added link fix for site

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
             <Route exact path="/" component={About} />
             <Route exact path="/about" component={About} />
+            <Route exact path="/react-portfolio" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/contact" component={Contact} />
           <Footer />
