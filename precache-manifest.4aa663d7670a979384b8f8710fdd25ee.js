809fbd09e4a5c2b7a5cd5bcf9edb973e2d1dd3f0
Jordan Triplett <JordanT111596@gmail.com>
2020-10-03 18:45:43 -0400

    Updates

--- precache-manifest.4aa663d7670a979384b8f8710fdd25ee.js
+++ precache-manifest.4aa663d7670a979384b8f8710fdd25ee.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "509de2a21548838b606e8478e16c6f74",
+    "revision": "a77214f833722bcf7121977737e7cc40",
     "url": "/react-portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "d5ff09fbe4721518ffca",
+    "revision": "ed30fb403d6eb0c3dcb8",
     "url": "/react-portfolio/static/css/main.93e94673.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.b4b48c7e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d5ff09fbe4721518ffca",
-    "url": "/react-portfolio/static/js/main.bda1e56e.chunk.js"
+    "revision": "ed30fb403d6eb0c3dcb8",
+    "url": "/react-portfolio/static/js/main.139a0428.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
