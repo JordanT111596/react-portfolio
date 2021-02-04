7890ab798db43407c1265e741cc8d2aba372103b
JordanT111596 <JordanT111596@gmail.com>
2021-02-03 21:14:34 -0500

    Created state for contact submission

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Contact() {
+
+    const [name, setName] = useState("Name");
+    const [email, setEmail] = useState("email@address.com");
+    const [message, setMessage] = useState("Message");
+
+
     return (
         // Grid Container
         <div className="container w-auto full-page pb-5">
@@ -18,30 +24,30 @@ function Contact() {
                             {/* A box to enter a name */}
                             <div className="form-group">
                                 <label for="exampleFormControlTextarea1">Name</label>
-                                <textarea className="form-control" id="exampleFormControlTextarea1" rows="1"
-                                    placeholder="Name"></textarea>
+                                <input className="form-control" id="name" value={name} onChange={e => setName(e.target.value)}
+                                    placeholder="Name"></input>
                             </div>
                             {/* A box to enter an email address */}
                             <div className="form-group">
                                 <label for="exampleFormControlInput1">Email address</label>
-                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" />
+                                <input type="email" className="form-control" id="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
                             </div>
-                                {/* A box to enter a message */}
-                                <div className="form-group">
-                                    <label for="exampleFormControlTextarea1">Message</label>
-                                    <textarea className="form-control" id="exampleFormControlTextarea2" rows="3"
-                                        placeholder="Message"></textarea>
-                                </div>
-                                {/* A button to submit the information */}
-                                <button type="submit" className="btn btn-primary">Submit</button>
+                            {/* A box to enter a message */}
+                            <div className="form-group">
+                                <label for="exampleFormControlTextarea1">Message</label>
+                                <textarea className="form-control" id="message" rows="3" value={message} onChange={e => setMessage(e.target.value)}
+                                    placeholder="Message"></textarea>
+                            </div>
+                            {/* A button to submit the information */}
+                            <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
                     </div>
-                    </div>
-                    <div className="col-md-3">
-                        {/* Blank Space */}
-                    </div>
+                </div>
+                <div className="col-md-3">
+                    {/* Blank Space */}
                 </div>
             </div>
+        </div>
     );
 }
 
