8db8b908d3f59d6058723a3131a9706af84257de
Jordan Triplett <JordanT111596@gmail.com>
2021-02-04 19:06:53 -0500

    Added subject line to the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,6 +4,7 @@ function Contact() {
 
     //Using hooks for all things submitted to the form
     const [name, setName] = useState("");
+    const [subject, setSubject] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
 
@@ -14,9 +15,11 @@ function Contact() {
 
         //Creates a link that takes the form info from the state and prefills out an email message, waiting to be sent to me!
         var link = "mailto:JordanT111596@gmail.com"
-            + "?subject=" + encodeURIComponent("A new message from " + name + " using the React portfolio contact page!")
-            + "&body=" + encodeURIComponent(message + "\n\nPlease contact me back via email at " + email);
+            + "?subject=" + encodeURIComponent(subject)
+            + "&body=" + encodeURIComponent(message + "\n\nPlease contact me back via email at " + email + "\n\nThis message was sent from " + name + " using the React portfolio contact page!");
         window.location.href = link;
+
+        console.log(link);
     }
 
 
@@ -36,18 +39,24 @@ function Contact() {
                         <form onSubmit={handleFormSubmit}>
                             {/* A box to enter a name */}
                             <div className="form-group">
-                                <label htmlFor="exampleFormControlTextarea1">Name</label>
+                                <label htmlFor="Name">Name</label>
                                 <input className="form-control" id="name" value={name} onChange={e => setName(e.target.value)}
                                     placeholder="Name"></input>
                             </div>
+                            {/* A box to enter a subject */}
+                            <div className="form-group">
+                                <label htmlFor="Subject">Subject</label>
+                                <input className="form-control" id="Subject" value={subject} onChange={e => setSubject(e.target.value)}
+                                    placeholder="Subject"></input>
+                            </div>
                             {/* A box to enter an email address */}
                             <div className="form-group">
-                                <label htmlFor="exampleFormControlInput1">Email address</label>
+                                <label htmlFor="Email">Email address</label>
                                 <input type="email" className="form-control" id="email" placeholder="Email@address.com" value={email} onChange={e => setEmail(e.target.value)} />
                             </div>
                             {/* A box to enter a message */}
                             <div className="form-group">
-                                <label htmlFor="exampleFormControlTextarea1">Message</label>
+                                <label htmlFor="Message">Message</label>
                                 <textarea className="form-control" id="message" rows="3" value={message} onChange={e => setMessage(e.target.value)}
                                     placeholder="Message"></textarea>
                             </div>
