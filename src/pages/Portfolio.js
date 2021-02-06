e54c893d2f4a2a4d56bba730fee93c43edefb20d
Jordan Triplett <JordanT111596@gmail.com>
2021-02-05 19:16:29 -0500

    Lunch Randomizer finished

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -40,7 +40,8 @@ function Portfolio() {
                             university they attend, and a brief bio about themselves. Connect 
                             with other students, study together, and learn more today!"
                             tech="React.js, SCSS, react-bootstrap, react-animations, react-animate-on-scroll, JSX, 
-                        Pusher Notifications (WebSockets), MySQL Migrations, Node.js, Express.js, Passport (Authentication)" />
+                            Pusher Notifications (WebSockets), MySQL Migrations, Node.js, Express.js, 
+                            Passport (Authentication), HTML, CSS, Javascript" />
                         {/* Gif and Link to Pandemic Playlist */}
                         <Project name="Pandemic Playlist"
                             deployLink="https://thawing-beyond-59803.herokuapp.com"
@@ -52,14 +53,19 @@ function Portfolio() {
                             and password and you can curate your playlist based on age and genre! Add movies to
                             your playlist, your favorites list, or separate by the ones you've seen and the ones
                             you haven't! Want more info about a movie? We've got that for you too!"
-                            tech="Bootstrap CSS, Passport (Authentication), MySQL & Sequelize, Express.js, Node.js, The Movie DB API" />
+                            tech="Bootstrap CSS, Passport (Authentication), MySQL & Sequelize, Express.js, 
+                            Node.js, Handlebars, The Movie DB API, HTML, CSS, Javascript, jQuery" />
                         {/* <Gif and Link to the Lunch Randomizer */}
                         <Project name="Lunch Randomizer"
                             deployLink="https://danielgerrald.github.io/Project_1"
                             image={ranGen} alt="Demo Gif for Lunch Randomizer"
                             repoLink="https://github.com/DanielGerrald/Project_1"
-                            desc=""
-                            tech="" />
+                            desc="It’s 1:00pm and you just finished a hard project at work, you stand up and think to yourself,
+                            I am starving. You turn to your buddy and say, let’s get lunch. Then you both stare at each other 
+                            cause you know it’s hard to decide on a place you both want as well as taking into account what you 
+                            ate for lunch yesterday and you’ve eaten at most of the places close to your office.                 
+                            Say hello to your new best friend for picking where you will eat for lunch!"
+                            tech="Materialize CSS, Zomato's API, Google Maps API, jQuery, HTML, CSS, Javascript" />
                         {/* Gif and Link to the Weather Dashboard */}
                         <Project name="Weather Dashboard"
                             deployLink="https://jordant111596.github.io/Weather-Dashboard"
