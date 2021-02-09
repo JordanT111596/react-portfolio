33a7f781600c2c8e72afd8211658639207b1f42d
JordanT111596 <JordanT111596@gmail.com>
2021-02-09 16:46:40 -0500

    Commented out new hamburger, will change implementation

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,20 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-//Serves as my "header" for assignment requirements
-
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/react-portfolio/">
         Jordan Triplett
         </Link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#globalNavbar" aria-controls="globalNavbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLinks"
+      aria-controls="navbarLinks" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse w-100 order-3" id="globalNavbar">
+      <div className="collapse navbar-collapse w-100 order-3" id="navbarLinks"> */}
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <Link
@@ -42,7 +41,7 @@ function Navbar() {
             </Link>
           </li>
         </ul>
-      </div>
+      {/* </div> */}
     </nav>
   );
 }
