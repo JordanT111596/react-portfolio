a4e8c4bd0882fe11c5d5dc93ae1732083baf23e8
Jordan Triplett <JordanT111596@gmail.com>
2021-02-10 17:51:37 -0500

    Can now manipulate state to collapse navbar, button causes re-render crash

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
+
+  const [navCollapse, setNavCollapse] = useState(true);
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/react-portfolio/">
         Jordan Triplett
-        </Link>
-      {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLinks"
-      aria-controls="navbarLinks" aria-expanded="false" aria-label="Toggle navigation">
+      </Link>
+      <button className="navbar-toggler" type="button" aria-expanded="false" 
+      // onClick={setNavCollapse(!navCollapse)}
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className="collapse navbar-collapse w-100 order-3" id="navbarLinks"> */}
+      <div className={(navCollapse ? 'collapse' : '') + ' navbar-collapse w-100 order-3'} id="navbarLinks">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <Link
@@ -41,7 +45,7 @@ function Navbar() {
             </Link>
           </li>
         </ul>
-      {/* </div> */}
+      </div>
     </nav>
   );
 }
