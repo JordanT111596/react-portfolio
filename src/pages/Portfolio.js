c769c8e0d1ccf56bc613e2fe7fa176d81834056b
JordanT111596 <JordanT111596@gmail.com>
2021-02-15 14:37:31 -0500

    Updated StudyMate description

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -33,7 +33,7 @@ function Portfolio() {
                             deployLink="https://floating-journey-44508.herokuapp.com/"
                             image={studyMateDemo} alt="Demo Gif for StudyMate"
                             repoLink="https://github.com/collinlanie12/StudyMate"
-                            desc="StudyMate is a web application that allows students to meet 
+                            desc="StudyMate is a SERN-stack (SQL, Express, React, Node) web application that allows students to meet 
                             and study subjects together. Students can signup to our site and 
                             fill out their profile with information such as their major, 
                             current classes, what subject they are most proficient in, what 
